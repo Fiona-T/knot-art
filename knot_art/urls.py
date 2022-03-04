76a54e02721689f669af08e12bae6857cf4416f5
Fiona-T <fitracey@gmail.com>
2022-03-04 12:41:53 +0000

    build: create home app, add app to settings.py and include it in project urls.py

--- knot_art/urls.py
+++ knot_art/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 #  for Django to see MEDIA_URL from settings
 from django.conf import settings
 # Importing static function to use below to add MEDIA_URL to the list of urls
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
