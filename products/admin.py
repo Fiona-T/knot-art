5aac122f49547a80118f09f14099902c14fdcc7b
Fiona-T <fitracey@gmail.com>
2022-03-08 16:52:50 +0000

    feat: create CategoryAdmin class for admin site settings, products.admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
+"""Set up of django admin site for products app"""
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Category model - admin site set up:
+    Show all fields in list display
+    """
+    list_display = ('friendly_name', 'name')
