0ba8e367b51690a41a3bbcf6d4b959ff2b9826fd
Fiona-T <fitracey@gmail.com>
2022-03-08 15:27:36 +0000

    build: create Category and Product models in products app, run migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
+"""models for products app"""
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model for products"""
+
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """string method - return the category name"""
+        return self.name
+
+    def get_friendly_name(self):
+        """return the user friendly name - for views"""
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """Product model - for products in shop"""
+
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='products_in_category'
+        )
+    sku = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_new = models.BooleanField(default=True)
+
+    def __str__(self):
+        """string method - return the product name and category"""
+        return f'{self.name} in {self.category}'
