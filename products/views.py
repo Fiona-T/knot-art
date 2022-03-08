9cfbc66a7bcfdb8e163d7053e864236638b6733a
Fiona-T <fitracey@gmail.com>
2022-03-08 18:04:11 +0000

    feat: update show_products view to only retrieve products that are active, products.views.py

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 def show_products(request):
     """View to display the products in shop"""
-    products = Product.objects.all()
+    products = Product.objects.filter(is_active=True)
     context = {
         'products': products,
     }
