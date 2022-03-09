4a419868af690bdc994479ad65e6ba0b80acafff
Fiona-T <fitracey@gmail.com>
2022-03-09 18:42:03 +0000

    feat: update product_details view to restrict viewing inactive products to superuser, products.views.py

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 """Views for products app - shop pages, product admin"""
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Product
 
 
@@ -20,9 +21,16 @@ def show_products(request):
 
 
 def product_details(request, product_id):
-    """View to show individual product details from shop page"""
+    """
+    View to show individual product details from shop page
+    Superuser can access the view for any products
+    Other users can only access the view for active products
+    """
     product = get_object_or_404(Product, pk=product_id)
-    context = {
-        'product': product,
-    }
-    return render(request, 'products/product_details.html', context)
+    if not product.is_active and not request.user.is_superuser:
+        raise Http404
+    else:
+        context = {
+            'product': product,
+        }
+        return render(request, 'products/product_details.html', context)
