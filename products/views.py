f8b3f420bfe49e0da028ac766d5127911793cd86
Fiona-T <fitracey@gmail.com>
2022-03-10 17:22:02 +0000

    feat: update show_products view to handle search GET request, products.views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 """Views for products app - shop pages, product admin"""
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Q
 from .models import Product
 
 
@@ -8,14 +9,30 @@ def show_products(request):
     """
     View to display the products in shop
     Only active products are shown, unless user is superuser.
+    Products are then filtered if there is a search term
     """
     if request.user.is_superuser:
         products = Product.objects.all()
     else:
         products = Product.objects.filter(is_active=True)
 
+    search_term = None
+
+    # GET requests for search, categories and sorting
+    if request.GET:
+        # handles searches - q will be sent in the request url
+        if 'q' in request.GET:
+            search_term = request.GET['q']
+        matches_search = Q(
+            name__icontains=search_term
+            ) | Q(
+                description__icontains=search_term
+                )
+        products = products.filter(matches_search)
+
     context = {
         'products': products,
+        'search_term': search_term,
     }
     return render(request, 'products/products.html', context)
 
