e35f217de86b77659d0a00c32cce0b7e14c739c9
Fiona-T <fitracey@gmail.com>
2022-03-11 10:51:00 +0000

    feat: update show_products view to filter by category in get request, products.views.py

--- products/views.py
+++ products/views.py
@@ -2,14 +2,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def show_products(request):
     """
     View to display the products in shop
     Only active products are shown, unless user is superuser.
-    Products are then filtered if there is a search term
+    Products are then filtered if there is a get request with 'q'
+    Or filtered by category if get request with category 
     """
     if request.user.is_superuser:
         products = Product.objects.all()
@@ -17,22 +18,30 @@ def show_products(request):
         products = Product.objects.filter(is_active=True)
 
     search_term = None
+    category = None
 
     # GET requests for search, categories and sorting
     if request.GET:
-        # handles searches - q will be sent in the request url
+        # handles filtering by category
+        if 'category' in request.GET:
+            category = request.GET['category']
+            category = get_object_or_404(Category, name=category)
+            products = products.filter(category=category)
+
+        # handles searches
         if 'q' in request.GET:
             search_term = request.GET['q']
-        matches_search = Q(
-            name__icontains=search_term
-            ) | Q(
-                description__icontains=search_term
-                )
-        products = products.filter(matches_search)
+            matches_search = Q(
+                name__icontains=search_term
+                ) | Q(
+                    description__icontains=search_term
+                    )
+            products = products.filter(matches_search)
 
     context = {
         'products': products,
         'search_term': search_term,
+        'current_category': category,
     }
     return render(request, 'products/products.html', context)
 
