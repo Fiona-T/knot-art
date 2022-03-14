790d0c849a73c60727d4004f75c85b56e40f5bbb
Fiona-T <fitracey@gmail.com>
2022-03-14 17:38:50 +0000

    feat: add script for quantity + and - buttons in product_details page, to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -24,7 +24,38 @@ function selectBoxSorting() {
     }
 }
 
-/** initialise the sorting options in select dropdown on products page */
+/**
+ * If btn_qty exists, then listen for click on either increment or decrement btns.
+ * On click, prevent default, then get the data-item_id from the element, use this id
+ * to get the input element by id (each input/button has the product id on it)
+ * Increase or decrease the current value by 1.
+ * Credit: initial code from Code Institute, amended/refactored.
+ *  */ 
+function quantityButtons() {
+    if(document.querySelectorAll(".btn-qty")){
+        let incrementBtn = document.querySelectorAll(".increment-qty");
+        incrementBtn.forEach(button => button.addEventListener("click", function (event) {
+            event.preventDefault();
+            let item_id = this.getAttribute("data-item_id");
+            let input_element = document.getElementById(`id_qty_${item_id}`)
+            let old_value = parseInt(input_element.value);
+            input_element.value = ++ old_value;
+        }));
+
+        let decrementBtn = document.querySelectorAll(".decrement-qty");
+        decrementBtn.forEach(button => button.addEventListener("click", function (event) {
+            event.preventDefault();
+            let item_id = this.getAttribute("data-item_id");
+            let input_element = document.getElementById(`id_qty_${item_id}`)
+            let old_value = parseInt(input_element.value);
+            input_element.value = -- old_value;
+        }));
+    }
+}
+
+/** initialise the sorting options in select dropdown on products page, 
+ * and quantity buttons in product details/cart */
 document.addEventListener("DOMContentLoaded", function () {
     selectBoxSorting();
+    quantityButtons();
 });
