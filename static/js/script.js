7085493882336b49859989a528f465508852b7a4
Fiona-T <fitracey@gmail.com>
2022-03-15 10:00:22 +0000

    feat: add enableDisableQtyBtns function to disable the btns if outside the allowed range, script.js

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,6 @@
  * Otherwise, clear any sort and direction so that all results will be shown
  * Credit: initial code from Code Institute, amended/refactored
  *  */ 
-
 function selectBoxSorting() {
     if(document.getElementById("sorting-selector")) {
         document.getElementById("sorting-selector").addEventListener("change", function() {
@@ -43,9 +42,10 @@ function quantityButtons() {
 }
 
 /**
- * Change the value in the input box, either up or down depending on direction
+ * Change the value in the input box, either up or down depending on direction, then call
+ * enableDisableQtyBtns, passing itemId
  * @param {string} itemId the id of the product, to get the relevant input element
- * @param {string} direction - direction of change, either increment or decrement
+ * @param {string} direction the direction of change, either increment or decrement
  */
 function changeInputValue(itemId, direction) {
     let inputElement = document.getElementById(`id_qty_${itemId}`);
@@ -55,8 +55,23 @@ function changeInputValue(itemId, direction) {
     } else if(direction === "increment") {
         inputElement.value = ++ oldValue;
     }
+    enableDisableQtyBtns(itemId);
 }
 
+/**
+ * Set disabled attribute on quantity buttons to true or false, depending on whether
+ * the current value in the input box is inside or outside of the allowed range.
+ * Credit: initial code from Code Institute, amended/refactored.
+ * @param {string} itemId the id of the product, to get the relevant input element
+ */
+function enableDisableQtyBtns(itemId) {
+    let inputElement = document.getElementById(`id_qty_${itemId}`);
+    let currentValue = parseInt(inputElement.value);
+    let minusDisabled = currentValue < 2;
+    let plusDisabled = currentValue > 9;
+    document.getElementById(`decrement-qty_${itemId}`).disabled = minusDisabled;
+    document.getElementById(`increment-qty_${itemId}`).disabled = plusDisabled;
+}
 
 /** initialise the sorting options in select dropdown on products page, 
  * and quantity buttons in product details/cart */
