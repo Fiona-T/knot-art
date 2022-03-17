cf0f740718a59add0dc5ac7671325154bcff7aee
Fiona-T <fitracey@gmail.com>
2022-03-17 11:18:52 +0000

    feat: add signals.py in checkout app so that order totals update when lineitem added/deleted, added to apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,15 @@
+"""
+config for checkout app - added function to override ready
+method, to import signals module - used to listen for signals
+from OrderLineItem instance to call method to update Order instance
+"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        """ override ready method - import signals module """
+        import checkout.signals
