7fc12c472209bac2076addbb1252e0b20884b27b
Fiona-T <fitracey@gmail.com>
2022-03-17 13:53:21 +0000

    feat: add checkout view to show Order form or error if no cart, views.py in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+"""Views for checkout app - checkout page to complete a purchase"""
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    """
+    Show the checkout page with Order form
+    If nothing in cart, return to shop page with error message.
+    """
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(
+            request,
+            "Can't checkout as you don't have anything in your bag at the "
+            "moment! Add some items and try again."
+            )
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
