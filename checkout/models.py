2f0f3c5a7c399391ae078de1f692a347a9dbb83d
Fiona-T <fitracey@gmail.com>
2022-03-17 14:14:19 +0000

    feat: amend blank label for Country dropdown in Order model, models.py in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -20,7 +20,10 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
+    country = CountryField(
+        blank_label='Choose country from dropdown',
+        null=False,
+        blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0
