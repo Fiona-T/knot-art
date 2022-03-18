221a9b765366e97f1f0a642cabe0fcca437cbd8e
Fiona-T <fitracey@gmail.com>
2022-03-18 16:54:21 +0000

    feat: update checkout view to create stripe payment intent, return client key + public key to context, views.py in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,11 @@
 """Views for checkout app - checkout page to complete a purchase"""
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+
+import stripe
+
+from cart.contexts import cart_contents
 from .forms import OrderForm
 
 
@@ -8,8 +13,16 @@ def checkout(request):
     """
     Show the checkout page with Order form
     If nothing in cart, return to shop page with error message.
+    Get stripe variables, create payment intent using secret key.
+    Get client_secret back from the intent, pass it to context to confirm
+    payment, along with public key which is used to create element.
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
+
+    # error message if nothing in cart
     if not cart:
         messages.error(
             request,
@@ -18,10 +31,30 @@ def checkout(request):
             )
         return redirect(reverse('products'))
 
+    # stripe total must be an integer, set api key and create intent
+    stripe_total = round(cart_contents(request)['grand_total'] * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    print(intent)
+
     order_form = OrderForm()
+
+    # message alert if public key not set
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            'Stripe public key is missing. Did you forget to set it in your '
+            'environment?'
+            )
+
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
