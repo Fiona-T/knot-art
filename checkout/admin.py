1ba6d2be9a1b006a248372641517a5220d33f353
Fiona-T <fitracey@gmail.com>
2022-03-18 19:03:32 +0000

    feat: add admin set up for Order, OrderLineItem in admin.py for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
+"""admin set up for 'checkout' app, show OrderLineItem inside Orders"""
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    OrderLineItems items will show up inside the relevant Order
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin set up for Order model: List display, ordering, fields.
+    """
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = (
+        'order_number', 'date', 'delivery_cost', 'order_total', 'grand_total',
+        )
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'town_or_city', 'county',
+              'postcode', 'country', 'delivery_cost',
+              'order_total', 'grand_total',)
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
