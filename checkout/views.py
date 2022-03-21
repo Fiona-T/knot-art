8e43c0a492f0fb214425fec5af44edb6e1cea94a
Fiona-T <fitracey@gmail.com>
2022-03-21 18:34:20 +0000

    feat: add cache_checkout_data view in views.py, checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,9 @@
 """Views for checkout app - checkout page to complete a purchase"""
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+import json
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+    )
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -9,6 +13,30 @@ from cart.contexts import cart_contents
 from products.models import Product
 from .models import OrderLineItem, Order
 from .forms import OrderForm
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Cache the data in the webhooks process from stripe_elements.js
+    Before calling confirmCardPayment js, makes a post request to this view
+    This view modifies the Stripe payment intent, adding cart contents to
+    the metadata. Cart contents will be used by payment_intent_succeeded handler
+    to find the order in the database and create it if it's not there.
+    Response sent back to js - 200 means will call confirmCardPayment Stripe
+    method; 400 means page will reload to show the error message.
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
