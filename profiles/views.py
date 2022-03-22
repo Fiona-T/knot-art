7f7163fb8cd805f7b209a790a05b4814a0498ffc
Fiona-T <fitracey@gmail.com>
2022-03-22 17:20:27 +0000

    feat: update profile view to include form in context, prepopulated with user info, views.py, profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,19 @@
 """Views for profile app - user profile"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
-    """Show the user profile page"""
+    """
+    Show the user profile page with form pre-populated with saved
+    info.
+    """
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=user_profile)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+    }
 
     return render(request, template, context)
