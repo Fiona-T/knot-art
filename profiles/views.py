10436075155b3aa62f22ca988b91a3f4308df568
Fiona-T <fitracey@gmail.com>
2022-03-23 12:15:33 +0000

    feat: update profile view to handle posting UserProfile form, views.py in profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 """Views for profile app - user profile"""
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -9,13 +10,26 @@ from .forms import UserProfileForm
 def profile(request):
     """
     Show the user profile page with form pre-populated with saved
-    info.
+    info. 'on_profile_page' in context is for messages so that bag
+    information is not shown in toast msg when just updating profile.
+    If post request, update the profile with the data from the form.
     """
     user_profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(instance=user_profile)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile information was updated.')
+        else:
+            messages.error(request, 'Please check the form and submit again.')
+    else:
+        form = UserProfileForm(instance=user_profile)
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
