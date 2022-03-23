873b6615981b53769da0ba64c987d4f269e2908a
Fiona-T <fitracey@gmail.com>
2022-03-23 11:04:05 +0000

    feat: add login required decorator to profile view in views.py, profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 """Views for profile app - user profile"""
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     """
     Show the user profile page with form pre-populated with saved
