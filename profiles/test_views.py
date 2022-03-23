1ecd628af169858f14721d1308ab1f416fb8d4b9
Fiona-T <fitracey@gmail.com>
2022-03-23 17:24:34 +0000

    test: add tests for order history to TestProfileView, test_views.py in profiles app

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -2,6 +2,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
+from products.models import Category, Product
+from checkout.models import Order
 from .models import UserProfile
 
 
@@ -10,7 +12,9 @@ class TestProfileView(TestCase):
     @classmethod
     def setUpTestData(cls):
         """
-        Two test users - second one with information attached to profile
+        Two test users - second one with information attached to profile.
+        Category and Product instance so Order can be created. 2nd user
+        attached to the Order instance.
         """
         user1 = User.objects.create_user(
             username='Tester',
@@ -27,6 +31,30 @@ class TestProfileView(TestCase):
         profile.default_street_address1 = 'My Street'
         profile.default_town_or_city = 'My Town'
         profile.save()
+
+        Category.objects.create(
+            name='category_and_category',
+            friendly_name='Category & Category'
+        )
+
+        Product.objects.create(
+            category=Category.objects.get(id=1),
+            name='product name',
+            sku='44444',
+            description='product description',
+            price=50.00,
+        )
+
+        order = Order.objects.create(
+            full_name='Name',
+            user_profile=UserProfile.objects.get(id=2),
+            email='email@email.com',
+            phone_number=profile.default_phone_number,
+            street_address1=profile.default_street_address1,
+            town_or_city=profile.default_town_or_city,
+            country='IE',
+        )
+        order.save()
 
     def test_redirects_if_not_logged_in(self):
         """
@@ -144,3 +172,33 @@ class TestProfileView(TestCase):
             messages[0].message,
             'Your profile information was updated.'
             )
+
+    def test_zero_orders_in_context_for_user_with_no_orders(self):
+        """
+        Login user who doesn't have any orders. Go to profile page, confirm
+        length of orders in context is 0 and that the message stating they
+        have no orders is in the html.
+        """
+        self.client.login(username='Tester', password='SecretCode14')
+        response = self.client.get('/profile/')
+        self.assertEqual(str(response.context['user']), 'Tester')
+        self.assertEqual(len(response.context['orders']), 0)
+        self.assertContains(
+            response,
+            '<p class="fw-600 mb-5">You don\'t have any previous orders '
+            'to be displayed yet.</p>'
+            )
+
+    def test_order_history_shown_for_user_wtih_previous_orders(self):
+        """
+        Login the second user that has an order saved to their profile.
+        Go to profile page, test that user_profile on the order matches this
+        user, and that there is one order in the context (one created in setUp)
+        """
+        self.client.login(username='TesterTwo', password='SecretCode14!')
+        response = self.client.get('/profile/')
+        self.assertEqual(str(response.context['user']), 'TesterTwo')
+        self.assertTrue('orders' in response.context)
+        for order in response.context['orders']:
+            self.assertEqual(response.context['user'], order.user_profile.user)
+        self.assertEqual(len(response.context['orders']), 1)
