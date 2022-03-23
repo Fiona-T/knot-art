6b1897bd19811c3909f28788e8eb6ac9ce507913
Fiona-T <fitracey@gmail.com>
2022-03-23 14:11:38 +0000

    test: add tests for UserProfileForm to test_forms.py, profiles app

---
+++ profiles/test_forms.py
@@ -0,0 +1,52 @@
+"""Tests for forms in profiles app"""
+from django.test import TestCase
+from .forms import UserProfileForm
+
+
+class TestUserProfileForm(TestCase):
+    """Tests for the UserProfileForm"""
+    def test_explicitly_set_field_labels_exist(self):
+        """
+        Check labels are present for the fields where a label was explicitly
+        set in the form Meta class
+        """
+        form = UserProfileForm()
+        self.assertTrue(
+            form.fields['default_street_address1'].label is None or
+            form.fields['default_street_address1'].label == 'Default Street address 1'
+            )
+        self.assertTrue(
+            form.fields['default_street_address2'].label is None or
+            form.fields['default_street_address2'].label == 'Default Street address 2'
+            )
+
+    def test_placeholders_exist(self):
+        """
+        Test placeholders present and correct on fields that should have them
+        """
+        form = UserProfileForm()
+        placeholders = {
+            'default_postcode': 'Postal Code or Eircode',
+            'default_county': 'County, State or Locality',
+        }
+        for field in form.fields:
+            if field in placeholders.keys():
+                self.assertEqual(
+                    form.fields[field].widget.attrs['placeholder'],
+                    placeholders[field]
+                    )
+
+    def test_css_class_exists_on_fields(self):
+        """Test CSS class is present and correct on the form fields"""
+        form = UserProfileForm()
+        for field in form.fields:
+            if field == 'default_country':
+                self.assertEqual(
+                    form.fields[field].widget.attrs['class'],
+                    'order-form-input country-input'
+                    )
+            else:
+                self.assertEqual(
+                    form.fields[field].widget.attrs['class'],
+                    'order-form-input'
+                    )
