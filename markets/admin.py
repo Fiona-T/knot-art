e7ad8a60b0169f2e8d5ec922edd10d5adb4759a5
Fiona-T <fitracey@gmail.com>
2022-03-29 14:14:54 +0000

    feat: add admin classes for County and Market models in admin.py, markets app

--- markets/admin.py
+++ markets/admin.py
@@ -1,3 +1,23 @@
+"""Set up of django admin site for markets app"""
 from django.contrib import admin
+from .models import County, Market
 
-# Register your models here.
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    """
+    County model - admin site set up:
+    Show all fields in list display
+    """
+    list_display = ('friendly_name', 'name')
+
+
+@admin.register(Market)
+class MarketAdmin(admin.ModelAdmin):
+    """
+    Market model - admin set up: fields to show in list view,
+    Allow filtering by county, and search by name + website.
+    """
+    list_display = ('name', 'location', 'county', 'date', 'website',)
+    list_filter = ('county',)
+    search_fields = ['name', 'website', ]
