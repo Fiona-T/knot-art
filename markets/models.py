aeda48e8dce0ca8d5c2bcecdb4e11a9b1742e5f5
Fiona-T <fitracey@gmail.com>
2022-03-29 14:12:52 +0000

    build: create County and Market models in models.py and make migrations, markets app

--- markets/models.py
+++ markets/models.py
@@ -1,3 +1,45 @@
+"""Models for 'markets' app"""
 from django.db import models
 
-# Create your models here.
+
+class County(models.Model):
+    """
+    County model - to hold counties in Ireland plus Dublin codes.
+    Foreign key in Market model so markets list can be filtered by
+    standardised list of counties in Ireland + by Dublin areas.
+    """
+
+    class Meta:
+        """Specify correct plural so it doesn't appear as Countys"""
+        verbose_name_plural = 'Counties'
+
+    name = models.CharField(max_length=20, unique=True)
+    friendly_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        """string method - return the county name"""
+        return self.name
+
+    def get_friendly_name(self):
+        """return the user friendly name for views/frontend"""
+        return self.friendly_name
+
+
+class Market(models.Model):
+    """Details about the markets - date time etc."""
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    county = models.ForeignKey(
+        County, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='markets',
+        )
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    website = models.URLField(max_length=254,)
+
+    def __str__(self):
+        """string method - return the market name and date"""
+        return f'{self.name} on {self.date}'
