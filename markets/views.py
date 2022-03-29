1d6c5cd111684907982e6218a31ce89bc6784ea0
Fiona-T <fitracey@gmail.com>
2022-03-29 17:46:27 +0000

    feat: update show_markets view to show all markets for admin user, views.py in markets app

--- markets/views.py
+++ markets/views.py
@@ -1,13 +1,19 @@
 """Views for 'markets' app - craft markets seller will be attending"""
-from django.shortcuts import render
 import datetime
+from django.shortcuts import render
 from .models import Market
 
 
 def show_markets(request):
-    """Show markets with date of today or later"""
+    """
+    Show markets with date of today or later in date order
+    If superuser, show all markets, newest first
+    """
     today = datetime.date.today()
-    markets = Market.objects.filter(date__gte=today).order_by('date')
+    if request.user.is_superuser:
+        markets = Market.objects.all().order_by('-date')
+    else:
+        markets = Market.objects.filter(date__gte=today).order_by('date')
     context = {
         'markets': markets,
     }
