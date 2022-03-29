033029d50aa6c77fe7546ef2604a1381efed266f
Fiona-T <fitracey@gmail.com>
2022-03-29 16:45:29 +0000

    feat: update show_markets view to filter by date greater/equal to today, views.py, markets app

--- markets/views.py
+++ markets/views.py
@@ -1,11 +1,13 @@
 """Views for 'markets' app - craft markets seller will be attending"""
 from django.shortcuts import render
+import datetime
 from .models import Market
 
 
 def show_markets(request):
-    """Show the markets"""
-    markets = Market.objects.all()
+    """Show markets with date of today or later"""
+    today = datetime.date.today()
+    markets = Market.objects.filter(date__gte=today).order_by('date')
     context = {
         'markets': markets,
     }
