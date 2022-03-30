0b9ce196a9f25f6c998e2e0a8f08ce19da979fdd
Fiona-T <fitracey@gmail.com>
2022-03-30 14:05:02 +0000

    feat: create add_market view, views.py and url, urls.py, markets app

--- markets/urls.py
+++ markets/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_markets, name='markets'),
+    path('add/', views.add_market, name='add_market'),
 ]
--- markets/views.py
+++ markets/views.py
@@ -1,7 +1,10 @@
 """Views for 'markets' app - craft markets seller will be attending"""
 import datetime
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from .models import Market
+from .forms import MarketForm
 
 
 def show_markets(request):
@@ -18,4 +21,19 @@ def show_markets(request):
         'markets': markets,
     }
     template = 'markets/markets.html'
+    return render(request, template, context)
+
+
+@login_required()
+def add_market(request):
+    """
+    Show market form for admin user to add market. Raise 403 if not admin.
+    """
+    if not request.user.is_superuser:
+        raise PermissionDenied()
+    form = MarketForm()
+    context = {
+        'form': form,
+    }
+    template = 'markets/add_market.html'
     return render(request, template, context)
