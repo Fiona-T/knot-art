9e87b44a9918b2220299ca49b9a39ccb007beab2
Fiona-T <fitracey@gmail.com>
2022-03-30 09:49:44 +0000

    test: add tests for date_passed property to TestMarketModel, test_models.py, markets app

--- markets/test_models.py
+++ markets/test_models.py
@@ -1,4 +1,5 @@
 """Unit tests for Models in 'markets' app"""
+import datetime
 from django.test import TestCase
 from django.db import IntegrityError
 from .models import County, Market
@@ -14,7 +15,7 @@ class TestCountyModel(TestCase):
             friendly_name='Dublin 2'
         )
 
-    def test_countyy_name_is_unique_in_table(self):
+    def test_countyy_name_must_be_unique_in_table(self):
         """
         Check that creating another record with the same name
         as the instance created in the setUp raises an error
@@ -51,17 +52,41 @@ class TestMarketModel(TestCase):
     """tests for Market model in markets app"""
     @classmethod
     def setUpTestData(cls):
-        """Create instance of County + Market models for tests"""
+        """
+        Create instance of County for creating Market instances.
+        Create 3 instances of Market: 1 with specific date, 1 dated today,
+        1 dated yesterday so can test date_passed property.
+        """
         County.objects.create(
             name='dublin_2',
             friendly_name='Dublin 2'
         )
+
+        today = datetime.date.today()
 
         Market.objects.create(
             name='The Craft Market',
             location='The Street',
             county=County.objects.get(id=1),
             date='2022-06-01',
+            start_time='09:00',
+            end_time='17:00',
+            website='www.crafted.ie',
+        )
+        Market.objects.create(
+            name='The Craft Market',
+            location='The Street',
+            county=County.objects.get(id=1),
+            date=today,
+            start_time='09:00',
+            end_time='17:00',
+            website='www.crafted.ie',
+        )
+        Market.objects.create(
+            name='The Craft Market',
+            location='The Street',
+            county=County.objects.get(id=1),
+            date=today - datetime.timedelta(days=1),
             start_time='09:00',
             end_time='17:00',
             website='www.crafted.ie',
@@ -74,3 +99,13 @@ class TestMarketModel(TestCase):
         """
         market = Market.objects.get(id=1)
         self.assertEqual(str(market), 'The Craft Market on 2022-06-01')
+
+    def test_date_passed_property_returns_true_if_date_passed(self):
+        """Market id 3 has date of yesterday, confirm date_passed is true"""
+        market = Market.objects.get(id=3)
+        self.assertTrue(market.date_passed)
+
+    def test_date_passed_property_returns_false_if_date_not_passed(self):
+        """Market id 2 has date of today, confirm date_passed is false"""
+        market = Market.objects.get(id=2)
+        self.assertFalse(market.date_passed)
