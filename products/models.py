ec02b9ef076b330b3c3d200b9298255b19daebcc
Fiona-T <fitracey@gmail.com>
2022-04-06 09:20:13 +0000

    feat: add generate_sku method to Product, and post_save method to add sku to instance, models.py in products app

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 """models for products app"""
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Category(models.Model):
@@ -23,7 +25,6 @@ class Category(models.Model):
 
 class Product(models.Model):
     """Product model - for products in shop"""
-
     category = models.ForeignKey(
         'Category',
         null=True,
@@ -31,7 +32,7 @@ class Product(models.Model):
         on_delete=models.SET_NULL,
         related_name='products_in_category'
         )
-    sku = models.CharField(max_length=50)
+    sku = models.CharField(max_length=10, null=False, editable=False)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -40,6 +41,25 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     is_new = models.BooleanField(default=True)
 
+    def generate_sku(self):
+        """generate sku, called by post save method add_sku_when_created"""
+        leading_zeros = '0' * (6 - len(str(self.pk)))
+        return f'{self.category.name[:3].upper()}-{leading_zeros}{self.pk}'
+
     def __str__(self):
         """string method - return the product name and category"""
         return f'{self.name} in {self.category}'
+
+
+@receiver(post_save, sender=Product)
+def add_sku_when_created(sender, instance, created, **kwargs):
+    """
+    Each time instance of Product is created, call generate_sku method to
+    add the sku to the product.
+    Method uses instance id, so needs to be done post save.
+    Only one signal so including here in model file instead of signals.py file.
+    """
+    if created:
+        if not instance.sku:
+            instance.sku = instance.generate_sku()
+            instance.save()
