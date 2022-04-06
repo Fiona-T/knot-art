df5d642abd41f5e4cc2d1c4b34869a9775de21ba
Fiona-T <fitracey@gmail.com>
2022-04-06 13:32:48 +0000

    feat: create UserProfileAdmin and inlines for Order, SavedMarketList in admin.py, profiles app

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,35 @@
+"""Set up of django admin site for profiles app"""
 from django.contrib import admin
+from checkout.models import Order
+from .models import UserProfile, SavedMarketList
 
-# Register your models here.
+
+class OrderAdminInline(admin.TabularInline):
+    """
+    Order instances to be shown inside the UserProfile.
+    Fields are read only, with link to view/edit the full Order
+    """
+    model = Order
+    show_change_link = True
+    readonly_fields = (
+        'order_number', 'date', 'full_name', 'email', 'grand_total'
+        )
+    fields = ['date', 'full_name', 'email', 'grand_total']
+
+
+class SavedMarketListAdminInline(admin.TabularInline):
+    """
+    SavedMarketList to be shown inside the UserProfile.
+    """
+    model = SavedMarketList
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin set up for UserProfile model - show the two inline models
+    so user's Orders and SavedMarketList are accessible from this view
+    """
+    inlines = (OrderAdminInline, SavedMarketListAdminInline,)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
