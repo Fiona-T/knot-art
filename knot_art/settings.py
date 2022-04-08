821c32adb11a9216ace214f13c2efac49e30def7
Fiona-T <fitracey@gmail.com>
2022-04-08 12:45:42 +0000

    build: add email settings for production to settings.py

--- knot_art/settings.py
+++ knot_art/settings.py
@@ -115,9 +115,6 @@ AUTHENTICATION_BACKENDS = [
 # for allauth
 SITE_ID = 1
 
-# temporarily log emails to console during development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # allauth settings - authenticate + verify with email, min username, login urls
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -230,4 +227,16 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Emails
-DEFAULT_FROM_EMAIL = 'knotart@example.com'
+if 'DEVELOPMENT' in os.environ:
+    # log emails to the console in development, dummy email address
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'knotart@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
