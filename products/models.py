ba7bb0640cb2b3e004d550d8103fcafee32e6f49
Fiona-T <fitracey@gmail.com>
2022-04-14 12:12:43 +0000

    fix: remove image_url from Product model, not required or being used, models.py and run migrations, products app

--- products/models.py
+++ products/models.py
@@ -36,7 +36,6 @@ class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_new = models.BooleanField(default=True)
