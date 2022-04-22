0a2fbdabceca0b00fff498a33e7943f7b4713931
Fiona-T <fitracey@gmail.com>
2022-04-22 19:15:04 +0000

    fix: update add_to_cart view to check if total for item will be increased above max, views.py, cart app

--- cart/views.py
+++ cart/views.py
@@ -15,7 +15,9 @@ def add_to_cart(request, item_id):
     """
     Add item to the cart. Get quantity from posted form.
     Get cart from session, if not there then create cart empty dictionary.
-    If item already in cart, increase quantity, otherwise add item to cart.
+    If item already in cart, check if total will be above max of 10, if yes
+    then show error, else increase quantity.
+    If item not already in cart then add it to the cart.
     Overwrite the session variable cart with new cart.
     Use 'redirect_url' hidden input in form to redirect user back to same page.
     """
@@ -24,12 +26,22 @@ def add_to_cart(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
     if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(
-            request,
-            f'Quantity for {product.name} updated to {cart[item_id]}',
-            extra_tags='show_bag_in_toast'
-            )
+        if cart[item_id] + quantity > 10:
+            messages.error(
+                request,
+                'As these are handmade items, I only sell a max of 10 of each '
+                f'at a time. You have {cart[item_id]} of "{product.name}" '
+                f'in your bag and adding another {quantity} will bring total '
+                f'quantity for that item above 10. Thank you for your interest'
+                ' but please reduce the quantity and try again. Thank you!'
+                )
+        else:
+            cart[item_id] += quantity
+            messages.success(
+                request,
+                f'Quantity for {product.name} updated to {cart[item_id]}',
+                extra_tags='show_bag_in_toast'
+                )
     else:
         cart[item_id] = quantity
         messages.success(
